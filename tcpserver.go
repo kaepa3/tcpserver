b81d93272a0e6b5b955652e355d56cd4ad41d8e4
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2016-06-23 23:43:43 +0900

    true first commit

---
+++ tcpserver.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"time"
+)
+
+// ConfigData is json format
+type ConfigData struct {
+	Port string `json:"port"`
+}
+
+func main() {
+	setting := settingGet()
+	tcpAddr, err := net.ResolveTCPAddr("tcp", setting.Port)
+	checkError(err)
+	listner, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listner.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func settingGet() ConfigData {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	var datasets ConfigData
+	jsonErr := json.Unmarshal(file, &datasets)
+	if jsonErr != nil {
+		fmt.Println("Format Error: ", jsonErr)
+		panic(err)
+	}
+	return datasets
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	fmt.Println("client accept!")
+	messageBuf := make([]byte, 1024)
+	messageLen, err := conn.Read(messageBuf)
+	checkError(err)
+
+	message := string(messageBuf[:messageLen])
+	message = message + " too!"
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	conn.Write([]byte(message))
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fatal: error: %s", err.Error())
+		os.Exit(1)
+	}
+}
