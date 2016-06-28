854399344d6157e74866eed0708d7741fee9e696
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2016-06-29 01:19:04 +0900

    fix logging

--- tcpserver.go
+++ tcpserver.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"time"
 
+	log "github.com/cihub/seelog"
+	"github.com/kaepa3/btext"
 	"github.com/kaepa3/cycle"
 )
 
@@ -27,7 +29,31 @@ type ConfigData struct {
 	Health HealthFile `json:"health"`
 }
 
+func initLogger() {
+	logConfig := `
+	<seelog type="adaptive" mininterval="200000000" maxinterval="1000000000" critmsgcount="5">
+		<formats>
+		    <format id="main" format="Time:%Date(2006/01/02) %Time	file:%File	func:%FuncShort	line:%Line	level:%LEV	msg:%Msg%n" />
+		    <format id="con" format="%Msg%n" />
+		</formats>
+		<outputs formatid="main">
+			<rollingfile filename="log.log" type="size" maxsize="102400" maxrolls="1" formatid = "main"/>
+			<console formatid = "con"/>
+		</outputs>
+	</seelog>`
+	logger, err := log.LoggerFromConfigAsBytes([]byte(logConfig))
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	log.ReplaceLogger(logger)
+}
+func logging(text string) {
+	log.Info(text)
+}
+
 func main() {
+	initLogger()
 	config = settingGet("config.json")
 	tcpAddr, err := net.ResolveTCPAddr("tcp", config.Port)
 	checkError(err)
@@ -70,9 +96,8 @@ func handleClient(conn net.Conn) {
 }
 
 func addFile() {
-	fmt.Println("call!")
-	contents, err := ioutil.ReadFile(config.Health.File) // ReadFileの戻り値は []byte
-	if err != nil {
+	contents := btext.BParseFile(config.Health.File)
+	if len(contents) != 0 {
 		sendQue.PushBack(contents)
 	}
 }
@@ -82,8 +107,8 @@ func revcivePacket(conn net.Conn) {
 	conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
 	messageLen, err := conn.Read(messageBuf)
 	if 0 == revcheckErr(err) {
-		message := string(messageBuf[:messageLen])
-		fmt.Println("comming -", message)
+		message := string(btext.TParseAry(messageBuf[:messageLen]))
+		logging("ooming\n" + message)
 	}
 }
 
@@ -94,7 +119,7 @@ func sendPacket(conn net.Conn) {
 		switch buff := message.(type) {
 		case []byte:
 			conn.Write(buff)
-			fmt.Println("Send")
+			logging("Send\n" + btext.TParseAry(buff))
 		}
 	}
 }
